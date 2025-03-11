94b58e6d8809e7ee693a4d3d950156bf643dd1e9
hamza <hmzthsan004@gmail.com>
2025-03-11 06:07:03 +0300

    Added Task16 component

---
+++ Tasks/Task16.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Task16 = () => {
+  return (
+    <View>
+      <Text>Task 16 Component</Text>
+    </View>
+  );
+};
+
+export default Task16;
